89ff89f23d923895025c29f1567f03f15f07e488
malkeazy <malkov.0704@gmail.com>
2026-06-01 18:05:16 +0300

    A solution using the .sort method.

--- MajorityElement/main.py
+++ MajorityElement/main.py
@@ -1,1 +1,9 @@
-print('hello')
+from itertools import count
+from typing import List
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums)//2]
+nums = [2,2,31,31,31,1, 1,2,2,2]
+sol = Solution()
+print(sol.majorityElement(nums))
