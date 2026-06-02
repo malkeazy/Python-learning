ff41b7c307ae3bc393a5384acc4984eb97b06149
malkeazy <malkov.0704@gmail.com>
2026-06-02 13:23:05 +0300

    added a solution using a single Set()

--- Intersection of Two Arrays/main.py
+++ Intersection of Two Arrays/main.py
@@ -1,8 +1,8 @@
 from typing import List
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return []
-nums1 = [1,2,2,1]
-nums2 = [2,2]
+        return list(set(nums1).intersection(nums2))
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
 sol = Solution()
 print(sol.intersection(nums1,nums2))
