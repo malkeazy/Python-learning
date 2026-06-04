90258fe48fba51bccb2a0c88c2a7ab2a14473dfd
malkeazy <malkov.0704@gmail.com>
2026-06-04 20:33:39 +0300

    change user consent

--- First File Uses Prog/main.py
+++ First File Uses Prog/main.py
@@ -1,17 +1,20 @@
 try:
-    notes_txt = open("data/notes.txt", 'r', encoding='utf-8')
-    notes = notes_txt.read()
-    if len(notes) > 0:
-        print(notes)
     while True:
-        user_consent = input("Хотите ли добавить заметку Y-yes N-no:")
-        if user_consent.upper() == "Y":
+        user_consent = input("1.Вывести все заметки\n"
+                             "2.Добавить заметку\n"
+                             "Выберете вариант:\n")
+        if user_consent == "1":
+            with open("data/notes.txt", 'r', encoding='utf-8') as notes_txt:
+                notes = notes_txt.readline()
+            if len(notes) > 0:
+                print(notes)
+            else:
+                print("Файл пустой")
+            break
+        elif user_consent.upper() == "2":
             with open("data/notes.txt", 'a', encoding='utf-8') as notes_txt:
                 notes_txt.write(input("Введите новую заметку:") + "\n")
             print("Заметка успешно добавлена")
-            break
-        elif user_consent.upper() == "N":
-            print("Файл остался без изменений")
             break
 except FileNotFoundError:
     print("Файл не найден")
