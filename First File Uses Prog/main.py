1702deddfd11d8105bc4d91afb5734c0f891aa4f
malkeazy <malkov.0704@gmail.com>
2026-06-05 15:14:58 +0300

    Minor fixes

--- First File Uses Prog/main.py
+++ First File Uses Prog/main.py
@@ -1,24 +1,24 @@
-try:
-    while True:
-        user_consent = input("1.Вывести все заметки\n"
+while True:
+    try:
+        user_consent = int(input("1.Вывести все заметки\n"
                              "2.Добавить заметку\n"
                              "0.Выход\n"
-                             "Выберете вариант:\n")
-        if int(user_consent) == 1:
+                             "Выберете вариант:\n"))
+        if user_consent == 1:
             with open("data/notes.txt", 'r', encoding='utf-8') as notes_txt:
                 notes = notes_txt.read()
             if len(notes) > 0:
                 print(notes)
             else:
                 print("Файл пустой")
-        elif int(user_consent) == 2:
+        elif user_consent == 2:
             with open("data/notes.txt", 'a', encoding='utf-8') as notes_txt:
                 notes_txt.write(input("Введите новую заметку:") + "\n")
             print("Заметка успешно добавлена")
-        elif int(user_consent) == 0:
+        elif user_consent == 0:
             break
-except FileNotFoundError:
-    print("Файл не найден")
-except ValueError:
-    print("Ошибка выбора")
+    except FileNotFoundError:
+        print("Файл не найден")
+    except ValueError:
+        print("Ошибка выбора")
 
