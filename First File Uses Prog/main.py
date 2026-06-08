0af5078fbce3ec74eb4791fa38b7f51757a71f8b
malkeazy <malkov.0704@gmail.com>
2026-06-08 18:23:46 +0300

    first vers uses json files

--- First File Uses Prog/main.py
+++ First File Uses Prog/main.py
@@ -1,24 +1,22 @@
+import json
 while True:
     try:
         user_consent = int(input("1.Вывести все заметки\n"
                              "2.Добавить заметку\n"
                              "0.Выход\n"
-                             "Выберете вариант:\n"))
+                             "Выберете вариант:"))
         if user_consent == 1:
-            with open("data/notes.txt", 'r', encoding='utf-8') as notes_txt:
-                notes = notes_txt.read()
-            if len(notes) > 0:
-                print(notes)
-            else:
-                print("Файл пустой")
+            with open("data/notes.json", 'r', encoding='utf-8') as notes_json:
+                notes = json.load(notes_json)
+            print(notes)
         elif user_consent == 2:
-            with open("data/notes.txt", 'a', encoding='utf-8') as notes_txt:
-                notes_txt.write(input("Введите новую заметку:") + "\n")
+            with open("data/notes.json", 'w', encoding='utf-8') as notes_json:
+                json.dump(input("Введите новую заметку:"), notes_json, indent=4, ensure_ascii=False)
             print("Заметка успешно добавлена")
         elif user_consent == 0:
             break
     except FileNotFoundError:
-        print("Файл не найден")
+        print("Файл не найден, попробуйте для начала добавить заметку")
     except ValueError:
         print("Ошибка выбора")
 
