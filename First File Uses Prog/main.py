d710779bf0a43980469c0c13a71c576abf479ebd
malkeazy <malkov.0704@gmail.com>
2026-06-10 15:29:25 +0300

    First File Uses Prog: logic improvement

--- First File Uses Prog/main.py
+++ First File Uses Prog/main.py
@@ -1,37 +1,48 @@
-import json, storage
+import storage, json
 
 while True:
     file_locate = "data/notes.json"
     past_notes = storage.return_past_notes(file_locate)
-    if past_notes is None:
-        print("файл поврежден")
-        break
 
     try:
-        user_consent = int(input("1.Вывести все заметки\n"
+        user_consent = int(input("\n1.Вывести все заметки\n"
                              "2.Добавить заметку\n"
                              "3.Удалить заметку\n"
                              "0.Выход\n"
                              "Выберете вариант:"))
         if user_consent == 1:
-            for number, note in enumerate(past_notes, start=1):
-                if number == 1:
-                    print(f"\n{number}: {note}")
-                elif number == len(past_notes):
-                    print(f"{number}: {note}\n")
-                else:
-                    print(f"{number}: {note}")
+            if len(past_notes) > 0:
+                for number, note in enumerate(past_notes, start=1):
+                    if number == 1:
+                        print(f"\n{number}: {note}")
+                    else:
+                        print(f"{number}: {note}")
+            else:
+                print("\nФайл еще не имеет заметок создайте пожалуйста")
         elif user_consent == 2:
-            storage.add_note(file_locate, input("\nВведите новую заметку:"))
+            status_add_note = storage.add_note(file_locate, input("\nВведите новую заметку:"))
+            if status_add_note:
+                print("\nЗаметка успешно добавлен")
+            else:
+                print("\nЗаметка не добавлен")
         elif user_consent == 3:
-            storage.delete_note(file_locate, int(input("\nВведите номер заметки:")))
+            if len(past_notes) > 0:
+                number_note = int(input("\nВведите номер заметки:"))
+                if number_note > 0:
+                    storage.delete_note(file_locate, number_note)
+                    print("\nЗаметка успешно удалена")
+                else:
+                    print("\nЗаметка не удалена, введен неверный номер")
+                    continue
+            else:
+                print("\nВ файле еще не сущевствует ни одной заметки")
         elif user_consent == 0:
             break
-    except FileNotFoundError:
-        print("\nФайл не найден, попробуйте для начала добавить заметку\n")
+    except json.JSONDecodeError:
+        print("\nОшибка, файл поврежден")
     except ValueError:
-        print("\nОшибка выбора\n")
+        print("\nОшибка выбора")
     except IndexError:
-        print("\nВведен неверный индекс\n")
+        print("\nВведен неверный индекс")
 
 
--- First File Uses Prog/storage.py
+++ First File Uses Prog/storage.py
@@ -1,6 +1,6 @@
 import json
 
-def return_past_notes(storage_file: str):
+def return_past_notes(storage_file: str) -> list:
     try:
         with open(storage_file, 'r', encoding='utf-8') as notes_json:
             past_notes = json.load(notes_json)
@@ -8,28 +8,17 @@ def return_past_notes(storage_file: str):
     except FileNotFoundError:
         past_notes = []
         return past_notes
-    except json.JSONDecodeError:
-        return None
 
-def add_note(storage_file: str, new_note: str):
+def add_note(storage_file: str, new_note: str) -> bool:
     past_notes = return_past_notes(storage_file)
     with open(storage_file, 'w', encoding='utf-8') as notes_json:
         past_notes.append(new_note)
         json.dump(past_notes, notes_json, indent=4, ensure_ascii=False)
-    print("\nЗаметка успешно добавлена\n")
+    return True
 
-def delete_note(storage_file: str, number_int: int):
+def delete_note(storage_file: str, number_int: int) -> bool:
     past_notes = return_past_notes(storage_file)
-    if number_int > 0:
-        del past_notes[number_int - 1]
-    else:
-        print("Заметка не удалена, введен неверный номер1")
-        return None
-
-    try:
-        with open(storage_file, 'w', encoding='utf-8') as notes_json:
-            json.dump(past_notes, notes_json, indent=4, ensure_ascii=False)
-        print("\nЗаметка успешно удалена\n")
-    except FileNotFoundError:
-        print("\nФайл не найден, попробуйте для начала добавить заметку\n")
-        return None
+    del past_notes[number_int - 1]
+    with open(storage_file, 'w', encoding='utf-8') as notes_json:
+        json.dump(past_notes, notes_json, indent=4, ensure_ascii=False)
+    return True
